c48cdc8dac4bfc88d275a9733778052d01e56182
ana261405 <ana.j.f.vasconcelos.1405@gmail.com>
2026-06-18 21:49:25 -0300

    Update cadastro

--- cadastros/cadastrar.php
+++ cadastros/cadastrar.php
@@ -6,6 +6,15 @@
 
 <!-- COMEÇAR FORMULÁRIO ANA JULIA --> 
 <!-- BORA PRA CIMA -->
-
+<form>
+    <label>Nome</label>
+    <input></input><br>
+    <label>Descrição</label>
+    <input></input><br>
+    <label>Preço</label>
+    <input></input><br>
+    <button type="submit">Cadastrar</button>
+   
+</form> 
 
 <?php include "../rodape.php"; ?>
