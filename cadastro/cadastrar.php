881751754282bb1724d26d3822aa7cf48e0e5bce
ana261405 <ana.j.f.vasconcelos.1405@gmail.com>
2026-06-21 12:56:34 -0300

    update: form cadastro

--- cadastro/cadastrar.php
+++ cadastro/cadastrar.php
@@ -1,39 +1,44 @@
 <?php include "../html/cabecalho.php"; ?>
 
 <h2>Bem vindo a página de cadastro</h2>
+<!-- caso não esteja logado tem que ter uma condição aqui *!!!* -->
 
-<!-- caso não esteja logado tem que ter uma condição aqui -->
+<!-- ao submeter esse form os campos vão seer validados -->
+<form id="cadastro" onsubmit="validarForm()">
 
 <!-- essa label esta vinculada ao select com o id categoria -->
 <label for="categoria">Categoria:</label>
-
-<select id="categoria" name="categoria">
+<select id="categoria" name="categoria" onchange="validarCategoria()">
     <option value= "1">Lanches</option> <!-- categoria 1 --> 
     <option value= "2">Bebidas</option> <!-- categoria 2 --> 
     <option value= "3">Porções</option> <!-- categoria 3 --> 
 </select>
 
-<form>
-    <label>Nome</label>
-    <input></input><br>
-    <label>Descrição</label>
-    <input></input><br>
-    <label>Preço</label>
-    <input></input><br>
-</form> 
+<br> 
+<label>Nome:</label>
+<input id="nome" type="text" placeholder="Nome do produto"></input><br>
+<label>Descrição:</label>
+<input id="descricao" type="text" placeholder="Descrição do produto"></input><br>
+<!-- o preço pode ser inserido com casas decimais e pode ser no minimo 0 -->
+<label>Preço:</label>
+<input id="preco" type="number" step="0.01" min="0" placeholder="Preço do produto"></input><br>
+<label>Imagem:</label>
+<input id="imagem" type="file" accept="image/*"></input><br>
+<!-- aceita apenas imagens -->
+
 
 <!-- caso seja uma bebiba tem que adicionar mais dois campos
 ($nome, $preco, $descricao, $volume, $recipiente, $imagem) -->
-<form id= "campoBebida">
-    <label>Volume</label>
-    <input></input><br>
-    <label>Recipiente</label>
-    <input></input><br>
-    <label>Imagem</label>
-    <input></input><br>
-</form> 
+<div id= "campoBebida">
+    <label>Volume:</label>
+    <input id="volume" type="number" step="0.01" min="0" placeholder="Volume da bebida"></input><br>
+    <label>Recipiente:</label>
+    <input id="recipiente" type="text" placeholder="Recipiente da bebida"></input><br>
+</div> 
 
-<button type="submit">Cadastrar</button>
+<button type="submit" form="cadastro">Cadastrar</button>
+
+</form>
 
 <script src="../js/cadastro.js"> </script> 
 <?php include "../html/rodape.php"; ?>
--- js/cadastro.js
+++ js/cadastro.js
@@ -1,14 +1,20 @@
 // primeiro para o formulario cadastrar
 
-const categoria = document.getElementById("categoria");
-const campoBebida = document.getElementById("campoBebida");
-//se a categoria for de bebida ele adiciona os campos de bebida
-
-//essa função significa se houver uma mudança no campo select ele verifica o valor da categoria
-categoria.addEventListener("change", function () {
+function validarCategoria(){
+    const categoria = document.getElementById("categoria");
+    const campoBebida = document.getElementById("campoBebida");
+    //se a categoria for de bebida ele adiciona os campos de bebida
     if(categoria.value == 2){
         document.getElementById("campoBebida").style.display = "block";
     }else{
         document.getElementById("campoBebida").style.display = "none";
     }
-});
+
+}
+
+function validarForm(){
+    const nome = document.getElementById("nome");
+    const preco = document.getElementById("preco");
+    const descricao = document.getElementById("descricao");
+    const imagem = document.getElementById("imagem");
+}
